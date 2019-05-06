75a701850917dc7eaab7c0488387d9d43f2710ba
brownjw1 <brownjw1@rose-hulman.edu>
2019-05-06 10:39:16 -0400

    DUNZO

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,8 +3,8 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Jared Brown.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -13,28 +13,71 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
 
+    root = tkinter.Tk()
+    frame1 = ttk.Frame(root, padding=20)
+    frame1.grid()
+    button1= ttk.Button(frame1, text='Click Me')
+    button1.grid()
+    button1['command'] = (lambda: printer())
+    button1.grid()
+
+    entry1 = ttk.Entry(frame1)
+    entry1.grid()
+    button2 = ttk.Button(frame1, text='Click Me')
+    button2.grid()
+    button2['command'] = (lambda: printer2(entry1))
+    button2.grid()
+
+    entry2 = ttk.Entry(frame1)
+    entry2.grid()
+    button3 = ttk.Button(frame1, text='Click Me')
+    button3.grid()
+    button3['command'] = (lambda: printer3(entry1,entry2))
+    button3.grid()
+
+
+    root.mainloop()
+
+def printer():
+    print('Hello')
+
+def printer2(entryBox):
+    contents=entryBox.get()
+    if(contents=='ok'):
+        print('Hello')
+    else:
+        print('Goodbye')
+
+def printer3(entryBox1,entryBox2):
+    for k in range(int(entryBox2.get())):
+        print(entryBox1.get())
+
+
+
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
 
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
 
+
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
 
+
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
